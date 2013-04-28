1ff503ec96a0c75eb4d023544ed7c4bf9ef7610d
weyewe <w.yunnal@gmail.com>
2013-04-29 00:49:29 +0700

    change the ERD

--- db/migrate/20130428145631_create_favourite_posts.rb
+++ db/migrate/20130428145631_create_favourite_posts.rb
@@ -1,7 +1,9 @@
 class CreateFavouritePosts < ActiveRecord::Migration
   def change
     create_table :favourite_posts do |t|
+      t.integer :sub_reddit_id 
       
+      t.string :name 
 
       t.timestamps
     end
